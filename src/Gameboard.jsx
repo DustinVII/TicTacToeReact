93de3327ffff05a81ae00e4e1b6320c9f0b69b9f
DustinVII <dustinrr@gmail.com>
2024-07-07 22:31:35 -0300

    modified:   src/Gameboard.jsx 	modified:   src/style.css

--- src/Gameboard.jsx
+++ src/Gameboard.jsx
@@ -7,7 +7,7 @@ export default function Gameboard() {
     { spotnumber: 2, type: null },
     { spotnumber: 3, type: null },
     { spotnumber: 4, type: null },
-    { spotnumber: 5, type: "o" },
+    { spotnumber: 5, type: null },
     { spotnumber: 6, type: null },
     { spotnumber: 7, type: null },
     { spotnumber: 8, type: null },
@@ -15,36 +15,49 @@ export default function Gameboard() {
   ]);
 
   const [playerTurn, setPlayerTurn] = useState("x");
+  const playerColor = (playerTurn === "x" ? "xcolor" : "ocolor");
 
   function addSpot(index) {
     setSpots((currentSpots) => {
       return currentSpots.map((spot, i) => 
 
           {if (i === index && spot.type === null) {
+
             return { ...spot, type: playerTurn };
+
+            
+
           } else {
             return spot;
           }}
 
       );
     });
+
     setPlayerTurn((prevTurn) => (prevTurn === "x" ? "o" : "x"));
+    
   }
 
     return (
+    
         <div id="gameboard">
+
+          <p>Player <span className={`upper ${playerColor}`}>{playerTurn}</span> it's your turn!</p>
             
             {spots.map((spot, index) => (
           <div 
             key={index} 
             className={`spot ${spot.spotnumber}`} 
-            onClick={() => addSpot(index)}
+            onClick={() => (spot.type === null ? addSpot(index) : undefined)}
           >
             {spot.type && <img src={`/src/assets/${spot.type}.png`} alt={spot.type} />}
           </div>
         ))}
 
+        
+
 
       </div>
+
     )
   }
