6c069ebbdd1b91fe7f5a074017600e485a6fe4e3
DustinVII <dustinrr@gmail.com>
2025-03-13 23:08:37 -0300

    Winning alert added

--- src/NewGame.js
+++ src/NewGame.js
@@ -6,11 +6,40 @@ import { ScoreContext } from './ScoreContext';
 
 const SecondPage = () => {
 
+  // indexes of spots that form winning combinations
+const winPatterns  = [
+  [0, 1, 2], // rows
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8], // diagonals
+  [2, 4, 6],
+];
+
+
+const checkWinner = (newSpotImages) => {
+  for (const [a, b, c] of winPatterns) {
+    if (
+      newSpotImages[a] !== null &&
+      newSpotImages[a] === newSpotImages[b] &&
+      newSpotImages[a] === newSpotImages[c]
+    ) {
+      return newSpotImages[a]; // Returns winning image URL or symbol
+    }
+  }
+  return null;
+};
+
+
+
         // Define the images you want to alternate
   const images = [
         'images/x.png',  // for example, player X
         'images/o.png'   // for example, player O
       ];
+      
 
 
           // Create an array of 9 elements (for 9 spots), initially all null
@@ -28,10 +57,23 @@ const SecondPage = () => {
                 // Set the clicked spot to the current image (e.g., X or O)
                 newSpotImages[index] = images[currentImageIndex];
                 setSpotImages(newSpotImages);
+
+
+                const winner = checkWinner(newSpotImages);
+                const winnerSymbol = checkWinner(newSpotImages);
+                if (winnerSymbol) {  
+                  alert('Player wins!');
+                  setScore(score + 1);
+                }
+
+
             
                 // Toggle to the next image
                 setCurrentImageIndex((currentImageIndex + 1) % images.length);
               };
+
+
+              const winnerImage = checkWinner(spotImages);
 
 
               // Reset the board by setting all spots to null
@@ -41,6 +83,12 @@ const SecondPage = () => {
         // Optionally, reset the score if desired:
         // setScore(0);
       };
+
+
+
+
+
+
 
 
 
